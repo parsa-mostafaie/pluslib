7cc5b5ae9d27a4e74229d26cbf788d5bb0c85bbe
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-05-22 17:18:14 +0330

    unti_xss()

--- @security/security.php
+++ @security/security.php
@@ -112,3 +112,23 @@ function secure_form(secure_form_enum $st = secure_form_enum::gen)
     return false;
   }
 }
+
+function unti_xss($html)
+{
+  $dom = new DOMDocument();
+
+  $dom->loadHTML($html);
+
+  $script = $dom->getElementsByTagName('script');
+
+  $remove = [];
+  foreach ($script as $item) {
+    $remove[] = $item;
+  }
+
+  foreach ($remove as $item) {
+    $item->parentNode->removeChild($item);
+  }
+
+  return $dom->saveHTML();
+}
