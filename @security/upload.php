5b094acfb1b95e06e33fb4c37eab25c863725686
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-05-29 19:28:55 +0330

    Update upload.php

--- @security/upload.php
+++ @security/upload.php
@@ -1,60 +1,66 @@
 <?php
+
 //NOTE: THIS FILE IS ALWAYS PUBLIC
 function uploadFile_secure(
-    $name,
-    $max_size = 3145728,
-    $allowedTypes = [
-        'image/png' => 'png',
-        'image/jpeg' => 'jpg'
-    ],
-    $prefix = ''
+  $name,
+  $max_size = 3145728,
+  $allowedTypes = [
+    'image/png' => 'png',
+    'image/jpeg' => 'jpg'
+  ],
+  $prefix = ''
 ) {
 
-    if ($_FILES[$name]["error"] != 0) {
-        //stands for any kind of errors happen during the uploading
-        return null;
-    }
+  if ($_FILES[$name]["error"] != 0) {
+    //stands for any kind of errors happen during the uploading
+    return null;
+  }
 
-    $filepath = $_FILES[$name]['tmp_name'];
-    $fileSize = filesize($filepath);
-    $fileinfo = finfo_open(FILEINFO_MIME_TYPE);
-    $filetype = finfo_file($fileinfo, $filepath);
+  $filepath = $_FILES[$name]['tmp_name'];
+  $fileSize = filesize($filepath);
+  $fileinfo = finfo_open(FILEINFO_MIME_TYPE);
+  $filetype = finfo_file($fileinfo, $filepath);
 
-    if ($fileSize === 0) {
-        throw new Exception("The file is empty.", 400);
-    }
+  if ($fileSize === 0) {
+    throw new Exception("The file is empty.", 400);
+  }
 
-    if ($fileSize > $max_size) {
-        throw new Exception("The file is too large", 400);
-    }
+  if ($fileSize > $max_size) {
+    throw new Exception("The file is too large", 400);
+  }
 
-    if (!in_array($filetype, array_keys($allowedTypes))) {
-        throw new Exception("File not allowed.", 400);
-    }
+  if (!in_array($filetype, array_keys($allowedTypes))) {
+    throw new Exception("File not allowed.", 400);
+  }
 
-    $filename = uniqid('UPLOAD_' . $prefix, true);
-    // $filename = basename($filepath);
+  $filename = uniqid('UPLOAD_' . $prefix, true);
+  // $filename = basename($filepath);
 
-    $extension = $allowedTypes[$filetype];
-    $targetDirectory = $_SERVER['DOCUMENT_ROOT'] . web_url(c_url('/uploads'));
+  $extension = $allowedTypes[$filetype];
+  $targetDirectory = etc_urlOfUpload('/uploads'); // loc
 
-    $newFilepath = $targetDirectory . "/" . $filename . "." . $extension;
+  $newFilepath = $targetDirectory . "/" . $filename . "." . $extension;
 
-    if (!copy($filepath, $newFilepath)) { // Copy the file, returns false if failed
-        throw new Exception("Can't move file.");
-    }
-    unlink($filepath); // Delete the temp file
+  if (!copy($filepath, $newFilepath)) { // Copy the file, returns false if failed
+    throw new Exception("Can't move file.");
+  }
+  unlink($filepath); // Delete the temp file
 
-    return 'uploads/' . $filename . '.' . $extension;
+  return 'uploads/' . $filename . '.' . $extension;
 }
 function unlinkUpload($fname)
 {
-    if (!urlOfUpload($fname))
-        return;
-    unlink($_SERVER['DOCUMENT_ROOT'] . urlOfUpload($fname));
+  if (!urlOfUpload($fname))
+    return;
+  unlink(etc_urlOfUpload($fname));
 }
 
 function urlOfUpload($fname)
 {
-    return $fname ? c_url('/' . $fname) : null;
+  return $fname ? c_url('/') . $fname : null;
+}
+
+function etc_urlOfUpload($fname)
+{
+  return $_SERVER['DOCUMENT_ROOT'] . urlOfUpload($fname);
 }
