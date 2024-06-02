134779300c8683598139f738ffbcc5e476641f0a
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-06-02 18:40:04 +0330

    slugify()

--- @url.php
+++ @url.php
@@ -41,3 +41,46 @@ function form_processor_url($path, $dir = '/libs/custom/@form', $base = '')
   $full = $base . $dir . $path;
   return $full;
 }
+
+// function slugify($text, string $divider = '-')
+// {
+//   // replace non letter or digits by divider
+//   $text = preg_replace('~[^\pL\d]+~u', $divider, $text);
+
+//   // transliterate
+//   $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+//   // remove unwanted characters
+//   $text = preg_replace('~[^-\w]+~', '', $text);
+
+//   // trim
+//   $text = trim($text, $divider);
+
+//   // remove duplicate divider
+//   $text = preg_replace('~-+~', $divider, $text);
+
+//   // lowercase
+//   $text = strtolower($text);
+
+//   if (empty($text)) {
+//     return 'n-a';
+//   }
+
+//   return $text;
+// }
+
+function slugify($string, $separator = '-')
+{
+  $zwng = html_entity_decode('&zwnj;');
+
+
+  $string = trim($string);
+  $string = str_replace($zwng, ' ', $string);
+  $string = mb_strtolower($string, 'UTF-8');
+  $string = preg_replace("/[^a-z0-9_\-\sءاآؤئبپتثجچحخدذرزژسشصضطظعغفقكکگلمنوهی]/u", '', $string);
+  $string = preg_replace("/[\s\-_]+/", ' ', $string);
+  $string = preg_replace("/[\s_]/", $separator, $string);
+
+  return $string;
+
+}
