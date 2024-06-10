2463067165ecfe64c0aaeaaef487e3bf76e60f94
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-06-10 11:56:14 +0330

    Remove @model Example

--- @sql/MVC/@model.php
+++ @sql/MVC/@model.php
@@ -11,19 +11,14 @@ class Model
   protected Sql_DB $connection;
   protected Sql_Table $table;
   protected string $primaryKey = 'ID';
-  protected bool $incrementing = true;
+
+  // protected bool $incrementing = true;
 
   public bool $exists = false;
-
+  protected $id;
 
   public static function all($cols = ['*'])
   {
     return (new static)->table->SELECT($cols)->Run();
   }
-}
-
-class Users extends Model
-{
-  protected Sql_DB $connection = db();
-  protected Sql_Table $table = $this->connection->TABLE('users');
 }
