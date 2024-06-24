672710b0614a565d86ff91f168b849f8f89077d9
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-06-24 14:07:09 +0330

    Create /helpers.php

---
+++ helpers.php
@@ -0,0 +1,72 @@
+<?php
+
+defined('ABSPATH') || exit;
+
+function truncate($string, $chars = 50, $terminator = ' …')
+{
+  if (mb_strlen($string) < $chars) {
+    return $string;
+  }
+  $cutPos = $chars - mb_strlen($terminator);
+  $boundaryPos = mb_strrpos(mb_substr($string, 0, mb_strpos($string, ' ', $cutPos)), ' ');
+  return mb_substr($string, 0, $boundaryPos === false ? $cutPos : $boundaryPos) . $terminator;
+}
+
+function valueof($fv, $data)
+{
+  $_fv = $fv;
+  if (is_callable($fv)) {
+    $_fv = $_fv($data);
+  }
+  return $_fv;
+}
+
+function importJSON($file, $assoc = null, $depth = 512, $flags = 0)
+{
+  return json_decode(file_get_contents($file), $assoc, $depth, $flags);
+}
+
+
+function number_format_short($n, $precision = 1)
+{
+  if ($n < 900) {
+    // 0 - 900
+    $n_format = number_format($n, $precision);
+    $suffix = '';
+  } else if ($n < 900000) {
+    // 0.9k-850k
+    $n_format = number_format($n / 1000, $precision);
+    $suffix = 'K';
+  } else if ($n < 900000000) {
+    // 0.9m-850m
+    $n_format = number_format($n / 1000000, $precision);
+    $suffix = 'M';
+  } else if ($n < 900000000000) {
+    // 0.9b-850b
+    $n_format = number_format($n / 1000000000, $precision);
+    $suffix = 'B';
+  } else {
+    // 0.9t+
+    $n_format = number_format($n / 1000000000000, $precision);
+    $suffix = 'T';
+  }
+
+  // Remove unecessary zeroes after decimal. "1.0" -> "1"; "1.00" -> "1"
+  // Intentionally does not affect partials, eg "1.50" -> "1.50"
+  if ($precision > 0) {
+    $dotzero = '.' . str_repeat('0', $precision);
+    $n_format = str_replace($dotzero, '', $n_format);
+  }
+
+  return $n_format . $suffix;
+}
+
+if (!function_exists('dd')) {
+  function dd($obj)
+  {
+    echo "<pre>";
+    var_dump($obj);
+    echo "</pre>";
+    die;
+  }
+}
