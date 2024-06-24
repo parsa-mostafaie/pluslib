3fc76f56f26e997b257044503e4394d62030b0bc
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-06-24 13:45:37 +0330

    Update httplink.js

--- frontend/httplink.js
+++ frontend/httplink.js
@@ -1,13 +1,28 @@
 import useAjax from "./@ajax.js";
 
-document.querySelectorAll("a[http-method]").forEach((el) => {
-  let $method = el.getAttribute("http-method") ?? "GET";
-  let $lnk = el.getAttribute("href") ?? "./";
+export function httplinksInit(
+  res = (data) => undefined,
+  rej = (data) => undefined,
+  allway = () => undefined,
+  refreshOn = 0, // 0: Allway, 1: Success, -1: Failed
+  dyn_data = ($lnk, $method) => ({})
+) {
+  document.querySelectorAll("a[http-method]").forEach((el) => {
+    let $method = el.getAttribute("http-method") ?? "GET";
+    let $lnk = el.getAttribute("href") ?? "./";
 
-  el.addEventListener("click", (e) => {
-    e.preventDefault();
-    useAjax($lnk, {}, $method).finally(() => {
-      location.reload();
+    el.addEventListener("click", (e) => {
+      e.preventDefault();
+      useAjax($lnk, dyn_data($lnk, $method), $method)
+        .then((response) => {
+          res(response) && refreshOn === 1 && location.reload();
+        })
+        .catch((response) => {
+          rej(response) && refreshOn === -1 && location.reload();
+        })
+        .finally(() => {
+          refreshOn === 0 && location.reload();
+        });
     });
   });
-});
+}
