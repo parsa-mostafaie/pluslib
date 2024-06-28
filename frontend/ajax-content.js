895784c4289895c9f2af493650a07e8339332185
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-06-28 11:22:50 +0330

    Update ajax-content.js

--- frontend/ajax-content.js
+++ frontend/ajax-content.js
@@ -11,6 +11,8 @@ export function ajaxContentLoad(
   document.querySelectorAll($selector).forEach((container) => {
     let $contents = container.querySelectorAll("[ajax-content]");
 
+    ajaxContentReLoads(container, arguments);
+
     $contents.forEach(($content) => {
       let $lnk = $content.getAttribute("href") ?? "./";
       let $method = $content.getAttribute("http-method") ?? "GET";
@@ -21,7 +23,7 @@ export function ajaxContentLoad(
 
       function loaded(response) {
         $loading.classList.add("d-none");
-        $content.innerHTML = response;
+        addJSX(response, $content);
       }
 
       useAjax($lnk, dyn_data($content), $method, {}, $follow)
@@ -41,3 +43,44 @@ export function ajaxContentLoad(
 window.addEventListener("load", () => ajaxContentLoad());
 
 window.ajaxContentLoad = ajaxContentLoad;
+
+function addJSX($html, $content) {
+  let scrollTop = window.scrollY;
+  $html = `<div>${$html}</div>`;
+  let domp = new DOMParser();
+  let doc = domp.parseFromString($html, "text/html");
+  let $scripts = [];
+  doc.querySelectorAll("script").forEach((scriptTag) => {
+    let newS = document.createElement("script");
+    newS.textContent = scriptTag.textContent;
+    scriptTag.remove();
+    $scripts.push(newS);
+  });
+  $content.append(...doc.firstChild.childNodes);
+  document.body.append(...$scripts);
+  $scripts.forEach((script) => script.remove());
+  window.scrollY = scrollTop;
+}
+
+function ajaxContentReLoads(container = undefined, args = undefined) {
+  let $arr = [...document.querySelectorAll("[ajax-reload]:not([ar-eh])")];
+  if (container) {
+    $arr.filter((v) => container.matches(v.getAttribute("ajax-reload")));
+  }
+  $arr.forEach((ar) => {
+    ar.addEventListener("click", (ev) => {
+      function action() {
+        ajaxContentLoad(ar.getAttribute("ajax-reload"), ...(args ?? []));
+      }
+      if (ev.pluslib_wait) {
+        ev.pluslib_actions ??= [];
+        ev.pluslib_actions.push(action);
+      } else {
+        action();
+      }
+    });
+    ar.setAttribute("ar-eh", "set");
+  });
+}
+
+window.ajaxContentReLoads = ajaxContentReLoads;
