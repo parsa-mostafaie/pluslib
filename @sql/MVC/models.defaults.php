90cea641cbf5a5479a32b50a229c06b09f1388b0
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-07-04 09:45:54 +0330

    Update models.defaults.php

--- @sql/MVC/models.defaults.php
+++ @sql/MVC/models.defaults.php
@@ -9,4 +9,18 @@ class User extends BaseModel
 {
   protected $table = 'users';
   protected $id_field = 'ID';
+
+  static function current()
+  {
+    if(!\pluslib\Auth::canLogin()){
+      return null;
+    }
+    return new static(
+      \getCurrentUserInfo_prop(
+        (new static)->id_field
+      )
+    );
+  }
 }
+
+include_once 'auth.php';
