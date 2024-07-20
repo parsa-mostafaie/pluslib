e1215af37f495575a61c5c2cb992c436b3a3cb22
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-07-20 13:12:52 +0330

    Allow Custom Autoload Directories!

--- __autoload.php
+++ __autoload.php
@@ -1,18 +1,41 @@
 <?php
+function pls_autoload(string|null $namespace = null, string|null $dir = null)
+{
+  static $structures = [];
+
+  if(!is_null($namespace) && !is_null($dir)){
+    $structures[$namespace] = $dir;
+  }
+
+  return $structures;
+}
+
 spl_autoload_register(function ($class) {
-  if (!str_starts_with(strtolower($class), 'pluslib')) {
-    return; // not pluslib
+  $structure = pls_autoload();
+
+  foreach ($structure as $namespace => $dir) {
+    if (!str_starts_with(strtolower($class), strtolower($namespace))) {
+      continue;
+    }
+
+    $classPath = $dir . "/$class.php";
+    $classPathDefault = $dir . "/$class/" . basename($class) . ".php";
+    $classPath = regular_url($classPath);
+    $classPathDefault = regular_url($classPathDefault);
+
+    if (file_exists($classPath)) {
+      include_once $classPath;
+    } else if (file_exists($classPathDefault)) {
+      include_once $classPathDefault;
+    } else {
+      continue;
+    }
+
+    return;
   }
 
-  $classPath = __DIR__ . "/$class.php";
-  $classPathDefault = __DIR__ . "/$class/" . basename($class) . ".php";
-  $classPath = regular_url($classPath);
-
-  if (file_exists($classPath)) {
-    include_once $classPath;
-  }else if(file_exists($classPathDefault)){
-    include_once $classPathDefault;
-  } else {
-    //TODO: Show error in custom handler
-  }
+  // NO Autoload found!
 });
+
+// Default Autoloads: Pluslib
+pls_autoload('pluslib', __DIR__);
