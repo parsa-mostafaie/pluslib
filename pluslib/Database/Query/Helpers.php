ce472343a785d5e1d53edf08271780353e83ad55
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-07-20 14:05:18 +0330

    NormalizeValue: Check for Bools

--- pluslib/Database/Query/Helpers.php
+++ pluslib/Database/Query/Helpers.php
@@ -3,6 +3,8 @@ namespace pluslib\Database\Query;
 
 defined('ABSPATH') || exit;
 
+use pluslib\Database\Expression;
+
 final class Helpers
 {
   private function __construct()
@@ -17,13 +19,16 @@ final class Helpers
   }
   public static function NormalizeValue($raw)
   {
-    if ($raw instanceof \SExpression) {
+    if ($raw instanceof Expression) {
       return $raw->raw;
     } elseif ($raw === null) {
       return 'NULL';
+    } elseif (is_bool($raw)) {
+      return $raw ? '1' : '0';
     } elseif (is_string($raw) && $raw !== '?') {
       return "'" . addslashes($raw) . "'";
     }
+
     return $raw;
   }
 
