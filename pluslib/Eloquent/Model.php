e537107f4339f22ad59eef96c85fcb7bca7e1ee6
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-07-28 21:05:13 +0330

    Add class `Model`
    * A class that extends `BaseModel` Without additional features

---
+++ pluslib/Eloquent/Model.php
@@ -0,0 +1,19 @@
+<?php
+namespace pluslib\Eloquent;
+
+use pluslib\Eloquent\BaseModel;
+
+defined('ABSPATH') || exit;
+/**
+ * Model class
+ *
+ * @extends `BaseModel` Without additional features
+ * @source https://github.com/brucealdridge/BaseModel Forked From brucealdridge's Code
+ */
+
+/**
+ * A Base database model to extend with tables
+ */
+abstract class Model extends BaseModel
+{
+}
