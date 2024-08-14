410a4b052fe3b7f2cf5c51b51af86cd1f244114a
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-08-14 18:28:07 +0330

    Model Attribute Mutators

---
+++ pluslib/Eloquent/Attribute.php
@@ -0,0 +1,46 @@
+<?php
+namespace pluslib\Eloquent;
+
+defined('ABSPATH') || exit;
+
+class Attribute
+{
+  protected $getter, $setter;
+
+  public static function make(?callable $get = null, ?callable $set = null)
+  {
+    $instance = new static;
+
+    $instance->getter = $get;
+    $instance->setter = $set;
+
+    return $instance;
+  }
+
+  public function get()
+  {
+    if ($this->hasGetter())
+      return ($this->getter)();
+
+    return null;
+  }
+
+  public function hasSetter()
+  {
+    return !!$this->setter;
+  }
+
+  public function hasGetter()
+  {
+    return !!$this->getter;
+  }
+
+  public function set($value)
+  {
+    if ($this->hasSetter()) {
+      return ($this->setter)($value);
+    }
+
+    return null;
+  }
+}
